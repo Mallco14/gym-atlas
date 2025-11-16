3439eb4295b58888ab3230512fbf616e7fdd469f
RMCV <rcanchanyav@gmail.com>
2025-11-16 16:38:19 -0500

    Initial commit

---
+++ src/main/java/com/upc/gym_atlas/GymAtlasApplication.java
@@ -0,0 +1,13 @@
+package com.upc.gym_atlas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymAtlasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymAtlasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upc/gym_atlas/GymAtlasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.gym_atlas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymAtlasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
