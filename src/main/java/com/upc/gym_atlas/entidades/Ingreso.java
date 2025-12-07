b1fe80984b788b1014071082c1e42c8e1369e548
Carlos-Paredes <carlosparedes130298@gmail.com>
2025-12-06 20:56:38 -0500

    creacion rama ingreso

---
+++ src/main/java/com/upc/gym_atlas/entidades/Ingreso.java
@@ -0,0 +1,45 @@
+package com.upc.gym_atlas.entidades;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "ingresos")
+public class Ingreso {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_ingreso")
+    private Integer idIngreso;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "id_cliente", nullable = false)
+    private Cliente cliente;
+
+    @NotNull
+    @Column(name = "fecha_ingreso", nullable = false)
+    private LocalDate fechaIngreso;
+
+    @NotNull
+    @Column(name = "hora_ingreso", nullable = false)
+    private LocalTime horaIngreso;
+
+    @Column(name = "recepcionista", length = 100)
+    private String recepcionista;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+}
